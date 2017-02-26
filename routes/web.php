bc85e5ec65a7f618cd0ba56ef3eead99ca30284d
Alosha Kaminski <aloshakaminski@gmail.com>
2017-02-26 16:45:26 +0100

    init

---
+++ routes/web.php
@@ -0,0 +1,101 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+Route::get('/','Front@index');
+Route::get('/products','Front@products');
+Route::get('/products/details/{id}','Front@product_details');
+Route::get('/products/categories','Front@product_categories');
+Route::get('/products/brands','Front@product_brands');
+Route::get('/blog','Front@blog');
+Route::get('/blog/post/{id}','Front@blog_post');
+Route::get('/contact-us','Front@contact_us');
+Route::get('/login','Front@login');
+Route::get('/logout','Front@logout');
+Route::get('/cart','Front@cart');
+Route::get('/checkout',[
+  'middleware' => 'auth',
+  'uses' => 'Front@checkout'
+]);
+Route::get('/search/{query}','Front@search');
+Route::get('blade', function () {
+  $drinks = array('Vodka','Gin','Brandy');
+  return view('page', array('name' => 'The Raven', 'day' => 'Friday', 'drinks' => $drinks));
+});
+Route::get('/insert', function() {
+    App\Category::create(array('name' => 'Music'));
+    return 'category added';
+});
+Route::get('/read', function(){
+    $category = new App\Category();
+
+    $data = $category->all(array('name','id'));
+
+    foreach ($data as $list) {
+        echo $list->id . ' ' . $list->name . '';
+    }
+});
+Route::get('/update', function() {
+    $category = App\Category::find(6);
+    $category->name = 'HEAVY METAL';
+    $category->save();
+
+    $data = $category->all(array('name','id'));
+
+    foreach ($data as $list) {
+        echo $list->id . ' ' . $list->name . '';
+    }
+});
+Route::get('/delete', function() {
+    $category = App\Category::find(5);
+    $category->delete();
+
+    $data = $category->all(array('name','id'));
+
+    foreach ($data as $list) {
+        echo $list->id . ' ' . $list->name . '';
+    }
+});
+Route::post('/cart', 'Front@cart');
+// Authentication routes...
+Route::get('auth/login', 'Front@login');
+Route::post('auth/login', 'Front@authenticate');
+Route::get('auth/logout', 'Front@logout');
+
+// Registration routes...
+Route::post('/register', 'Front@register');
+
+// API routes...
+Route::get('/api/v1/products/{id?}', ['middleware' => 'auth.basic', function($id = null) {
+      if ($id == null) {
+          $products = App\Product::all(array('id', 'name', 'price'));
+      } else {
+          $products = App\Product::find($id, array('id', 'name', 'price'));
+      }
+      return Response::json(array(
+            'error' => false,
+            'products' => $products,
+            'status_code' => 200
+        ));
+}]);
+
+Route::get('/api/v1/categories/{id?}', ['middleware' => 'auth.basic', function($id = null) {
+    if ($id == null) {
+        $categories = App\Category::all(array('id', 'name'));
+    } else {
+        $categories = App\Category::find($id, array('id', 'name'));
+    }
+    return Response::json(array(
+                'error' => false,
+                'user' => $categories,
+                'status_code' => 200
+            ));
+}]);
